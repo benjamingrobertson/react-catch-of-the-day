4139ba00ec1f7ffe9162c834f27f9b4c0f705969
Ben Robertson <benjamingrobertson@me.com>
2017-03-31 09:58:07 -0400

    Connect firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCQOAUaepUjGjSShkJi2bqmoaqwkjLvcIs",
+    authDomain: "catch-of-the-day-ben-robertson.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-ben-robertson.firebaseio.com",
+});
+
+export default base;
