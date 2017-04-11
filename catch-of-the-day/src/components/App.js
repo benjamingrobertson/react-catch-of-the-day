b97de5e317d0cf7f8c2ad0ab92a6a3d0f924bad4
Ben Robertson <benjamingrobertson@me.com>
2017-04-11 09:17:22 -0400

    Remove from order and inventory

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,8 +12,10 @@ class App extends React.Component {
 
         this.addFish = this.addFish.bind(this);
         this.updateFish = this.updateFish.bind(this);
+        this.removeFish = this.removeFish.bind(this);
         this.loadSamples = this.loadSamples.bind(this);
         this.addToOrder = this.addToOrder.bind(this);
+        this.removeFromOrder = this.removeFromOrder.bind(this);
 
         // getInitialState
         this.state = {
@@ -63,10 +65,16 @@ class App extends React.Component {
     }
 
     updateFish(key, updatedFish) {
-        const fishes = this.state.fishes;
+        const fishes = {...this.state.fishes};
         fishes[key] = updatedFish;
 
         this.setState({fishes});
+    }
+
+    removeFish(key) {
+        const fishes = {...this.state.fishes};
+        fishes[key] = null;
+        this.setState({ fishes });
     }
 
     addToOrder(key) {
@@ -75,6 +83,12 @@ class App extends React.Component {
         // update or add the new number of fish ordered
         order[key] = order[key] + 1 || 1;
         this.setState({ order });
+    }
+
+    removeFromOrder(key) {
+        const order = {...this.state.order};
+        delete order[key];
+        this.setState({order})
     }
 
     render() {
@@ -93,10 +107,12 @@ class App extends React.Component {
                 <Order
                     fishes={this.state.fishes}
                     order={this.state.order}
+                    removeFromOrder={this.removeFromOrder}
                     params={this.props.params}
                 />
                 <Inventory
                     addFish={this.addFish}
+                    removeFish={this.removeFish}
                     loadSamples={this.loadSamples}
                     updateFish={this.updateFish}
                     fishes={this.state.fishes}
