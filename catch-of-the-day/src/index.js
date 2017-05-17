2a608126987d21031633c5475e8e26709f4a22d1
Ben Robertson <benjamingrobertson@me.com>
2017-05-17 15:29:22 -0400

    Add repo for github pages

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -8,9 +8,10 @@ import NotFound from './components/NotFound';
 
 import StorePicker from './components/StorePicker';
 
+const repo = `/${window.location.pathname.split('/')[1]}`
 const Root = () => {
     return (
-        <BrowserRouter>
+        <BrowserRouter basename={repo}>
             <div>
                 <Match exactly pattern="/" component={StorePicker} />
                 <Match pattern="/store/:storeId" component={App} />
